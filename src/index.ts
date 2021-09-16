c6077af42d32bc5565718877b0b83274962dfe50
Watchara Pongsri <giffyp@hotmail.com>
2021-09-16 23:35:59 +0700

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@
   # Web Site: https://www.rockdevper.com
 */
 
-import * as cronJob from 'node-cron'
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { AppConfig } from './constants/Constants';
 import { getMinerInfo} from './util/DuinoCoin';
 import { sendLineNotify } from './util/LineNotify';
-import { getPriceMarketCap } from './util/MarketCap';
+//import { getPriceMarketCap } from './util/MarketCap';
 
 
 // Market price from https://coinmarketcap.com/currencies/chia-network/
@@ -21,7 +20,7 @@ const runTask = async () => {
     // let marketPrice = parseFloat(pricevalue.replace(/[฿]/g, m => '').replace(/[$]/g, m => '').replace(/[,]/g, m => ''));
   
     var msg = await getMinerInfo();
-    if (AppConfig.ENABLE_LINE_NOTIFY === 'true')
+    if (Boolean(AppConfig.ENABLE_LINE_NOTIFY))
       sendLineNotify(`${msg}`);
   } catch (error: any) {
     console.log('Error:' + error.message)
